bf383a108bdf446d2017ee7f8fc2b22dead1adb0
KhunSiThu <khunsithuaung65@gmail.com>
2024-10-25 08:29:54 +0630

    commit 1

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php require("./template/header.html"); ?>
+<?php require("./template/navBar.html"); ?>
+
+<?php require("./template/sideBar-left.html"); ?>
+<?php require("./template/show-all.html"); ?>
+<?php require("./template/sideBar-right.html"); ?>
+
+<?php require("./template/footer.html"); ?>
